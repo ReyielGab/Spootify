0e24975261457d253a557716d94df65370d94ffa
reyielgab <dirty_west666@yahoo.com>
2021-04-26 10:38:30 +0800

    changes

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '136e7af32db24eadaff69dfb9a456333',
+    clientSecret: '0fcd8cb29db7420fa6ed26ad0d9a80a7'
   }
 }
